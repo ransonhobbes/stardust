5e1a8020581b478c2e6c463ee39f66ff5ea7a099
Ranson Hobbes <ransonhobbes@protonmail.com>
2021-10-26 16:33:15 +0100

    More renames

--- contracts/StarToken.sol
+++ contracts/StarToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 
-//  StarToken: ERC777-compatible fungible $STAR token
+//  StarToken: ERC777-compatible fungible wrapped star token
 //
 //    This contract implements a simple ERC777-compatible fungible token. It's deployed
 //    and owned by the Treasury. The Treasury mints and burns these tokens when it
