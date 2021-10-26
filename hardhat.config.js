9ba5f7a16337a362857228f45df312ab5050282f
Ranson Hobbes <ransonhobbes@protonmail.com>
2021-10-26 16:48:27 +0100

    Upgrade solidity, node packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -37,7 +37,7 @@ module.exports = {
             },
             // for everything else
             {
-                version: "0.8.4",
+                version: "0.8.9",
                 settings: {
                     optimizer: {
                         enabled: true,
